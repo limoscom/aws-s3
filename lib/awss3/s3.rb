9212d8e97a1b65e52f0099d34492f2d12e7ba22e
Drew Bomhof <drew.bomhof@limos.com>
2015-01-29 16:33:06 -0500

    Moved all required namespace references to AWSS3

--- lib/awss3/s3.rb
+++ lib/awss3/s3.rb
@@ -28,7 +28,7 @@ require 's3/connection'
 require 's3/authentication'
 require 's3/response'
 
-module AWS
+module AWSS3S3
   module S3
     UNSAFE_URI = /[^-_.!~*'()a-zA-Z\d;\/?:@&=$,\[\]]/n
 
@@ -45,17 +45,17 @@ module AWS
     end
 
     Base.class_eval do
-      include AWS::S3::Connection::Management
+      include AWSS3::S3::Connection::Management
     end
 
     Bucket.class_eval do
-      include AWS::S3::Logging::Management
-      include AWS::S3::ACL::Bucket
+      include AWSS3::S3::Logging::Management
+      include AWSS3::S3::ACL::Bucket
     end
 
     S3Object.class_eval do
-      include AWS::S3::ACL::S3Object
-      include AWS::S3::BitTorrent
+      include AWSS3::S3::ACL::S3Object
+      include AWSS3::S3::BitTorrent
     end
   end
 end
@@ -65,14 +65,14 @@ require_library_or_gem 'xmlsimple', 'xml-simple' unless defined? XmlSimple
 # If libxml is installed, we use the FasterXmlSimple library, that provides most of the functionality of XmlSimple
 # except it uses the xml/libxml library for xml parsing (rather than REXML). If libxml isn't installed, we just fall back on
 # XmlSimple.
-AWS::S3::Parsing.parser =
+AWSS3::S3::Parsing.parser =
   begin
     require_library_or_gem 'xml/libxml'
     # Older version of libxml aren't stable (bus error when requesting attributes that don't exist) so we
     # have to use a version greater than '0.3.8.2'.
     raise LoadError unless XML::Parser::VERSION > '0.3.8.2'
     $:.push(File.join(File.dirname(__FILE__), '..', '..', 'support', 'faster-xml-simple', 'lib'))
-    require_library_or_gem 'faster_xml_simple' 
+    require_library_or_gem 'faster_xml_simple'
     FasterXmlSimple
   rescue LoadError
     XmlSimple
